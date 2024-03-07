44939657ba1feabec37c8f5971edc49a6c5eb793
rushirajd25 <rushirajd25@gmail.com>
2024-03-07 21:40:26 +0530

    feat(products): allow administrator to view, create, update and delete products

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=15, decimal_places=2)
+    category = models.CharField(max_length=30)
+    public = models.BooleanField(default=True)
